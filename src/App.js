c04256a92b7d0360f850768ba8f65da9ccdddfe2
Papaya Delight <jasmineteters2020@gmail.com>
2021-04-05 20:18:54 -0600

    added searchbar with onchange

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { CardList } from './components/card-list/card-list.component'
+import { CardList } from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -7,28 +7,28 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        
-      ],
+      monsters: [],
+      searchField: '',
     };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({monsters: users}))
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
-  render(){
-  return (
-    <div className='App'>
-      <CardList monsters={this.state.monsters} />
-
-    </div>
-  );
+  render() {
+    return (
+      <div className='App'>
+        <input
+          type='search'
+          placeholder='search monsters'
+          onChange={(e) => this.setState({ searchField: e.target.value })} />
+        <CardList monsters={this.state.monsters} />
+      </div>
+    );
   }
 }
-
-
 
 export default App;
