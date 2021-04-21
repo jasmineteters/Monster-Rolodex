3cf6c4f8c26d9734048022a9e30ae3e7f9e0355a
Papaya Delight <jasmineteters2020@gmail.com>
2021-04-20 21:45:41 -0600

    making the project more reusable

--- src/App.js
+++ src/App.js
@@ -18,6 +18,9 @@ class App extends Component {
       .then((response) => response.json())
       .then((users) => this.setState({ monsters: users }));
   }
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
+  }
 
   render() {
     const { monsters, searchField } =  this.state;
@@ -27,7 +30,9 @@ class App extends Component {
 
     return (
       <div className='App'>
-        <SearchBox placeholder='search monsters' handleChange={(e) => this.setState({ searchField: e.target.value })} />
+        <SearchBox 
+        placeholder='search monsters' 
+        handleChange={this.handleChange} />
         <CardList monsters={filteredMonsters} />
       </div>
     );
